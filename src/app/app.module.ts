c4c30821300c2a916b9e99a9eee7141404beff8e
Amirtha <amirtha4501@gmail.com>
2020-03-19 19:22:54 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent }
+    ])
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+
+
+export class AppModule { 
+  
+}
---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'lenova',
+        price: 40000,
+        description: 'The wonderful pc for middle-class'
+    },
+    {
+        name: 'hp',
+        price: 50000,
+        description: 'The excellent pc for coders'
+    },
+    {
+        name: 'samsung',
+        price: 45000,
+        description: 'The excellent pc for gamers'
+    }
+];
